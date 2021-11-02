d5e42f915e5e42cd059eddeba50ed9d221a49529
gagaspbahar <gagaspbahar@gmail.com>
2021-11-03 03:00:27 +0700

    Permainan Sotong

---
+++ Praktikum 2/Squid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "game.h"
+
+typedef struct node* Address; 
+typedef struct node {
+    int value;
+    int no;
+    Address next;
+} Node;
+
+void Squid(char *input){
+
+}
